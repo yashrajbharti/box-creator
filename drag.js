f3f8c5b824bf584792f2e0ff9a3d949fe35541bc
Yash Raj <yashraj@Yashs-MacBook-Air.local>
2022-06-28 11:37:26 +0530

    radius slider added

--- drag.js
+++ drag.js
@@ -6,3 +6,13 @@ $('.box').resizable({
 function colorSelected (element) {
     document.getElementById('wrapper').style.background = element.value
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  document.getElementById("wrapper").style.borderRadius = this.value+"px";
+}
